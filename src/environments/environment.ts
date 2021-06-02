e42e90a6bb8cc95fc7418cb92aa29b0888c1404f
Douglas <douglas.vanreeuwyk@tuks.co.za>
2021-06-02 20:39:07 +0200

    gitIgnore Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,27 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAFpQOCQy42NzigYd5aPH3OSpbjvADJ0o0",
+    authDomain: "smartstudentnotebook.firebaseapp.com",
+    databaseURL: "https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "smartstudentnotebook",
+    storageBucket:  "smartstudentnotebook.appspot.com",
+    messagingSenderId:  "254968215542"
+  }
+};
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAFpQOCQy42NzigYd5aPH3OSpbjvADJ0o0",
+  authDomain: "smartstudentnotebook.firebaseapp.com",
+  databaseURL: "https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "smartstudentnotebook",
+  storageBucket: "smartstudentnotebook.appspot.com",
+  messagingSenderId: "254968215542",
+  appId: "1:254968215542:web:be0931c257ad1d8a60b9d7",
+  measurementId: "G-YDRCWDT5QJ"
 };
 
 /*
