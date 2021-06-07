0f734c9faf5ea383d575788ed007a6d7c8287cc1
LouwC <u18000402@tuks.co.za>
2021-06-07 18:37:11 +0200

    Create backend file

---
+++ backend/user/UserServiceImpl.ts
@@ -0,0 +1,12 @@
+import {UserServiceInterface} from "./UserServiceInterface";
+import {LoginRequest} from "./request/LoginRequest";
+import {LoginResponse} from "./response/LoginResponse";
+
+export class UserServiceImpl implements UserServiceInterface
+{
+	Login(request: LoginRequest): LoginRequest
+	{
+		return null;
+	}
+}
+
