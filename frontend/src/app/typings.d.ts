3eb234847690f1b1506589b7afee7c0d18a52fc8
LouwC <u18000402@tuks.co.za>
2021-06-07 18:32:25 +0200

    Fixed file structure

---
+++ frontend/src/app/typings.d.ts
@@ -0,0 +1,5 @@
+declare module '@ckeditor/ckeditor5-build-decoupled-document' {
+  const DecoupledEditorBuild: any;
+
+  export = DecoupledEditorBuild;
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAFpQOCQy42NzigYd5aPH3OSpbjvADJ0o0",
+    authDomain: "smartstudentnotebook.firebaseapp.com",
+    databaseURL: "https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "smartstudentnotebook",
+    storageBucket:  "smartstudentnotebook.appspot.com",
+    messagingSenderId:  "254968215542",
+    appId: "1:254968215542:web:be0931c257ad1d8a60b9d7",
+    measurementId: "G-YDRCWDT5QJ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
