0ab1e88333c0712dc5c135fe7fc8f23983591947
LouwC <u18000402@tuks.co.za>
2021-06-08 23:12:27 +0200

    Deleted unnecessary files

--- backend/user/UserServiceInterface.ts
+++
@@ -1,7 +0,0 @@
-import {LoginRequest} from "./request/LoginRequest";
-import {LoginResponse} from "./response/LoginResponse";
-
-export interface UserServiceInterface
-{
-	Login(request: LoginRequest): LoginResponse;
-}
