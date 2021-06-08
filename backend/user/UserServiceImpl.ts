3a6d4b2aa322ca19c17555354842675f61c2e6b3
Luca <Lucaprenzler1@gmail.com>
2021-06-08 13:03:23 +0200

    Added The files to register a user aswell as created the service implementation

--- backend/user/UserServiceImpl.ts
+++ backend/user/UserServiceImpl.ts
@@ -1,10 +1,16 @@
 import {UserServiceInterface} from "./UserServiceInterface";
 import {LoginRequest} from "./request/LoginRequest";
 import {LoginResponse} from "./response/LoginResponse";
+import {RegisterRequest} from "./request/RegisterRequest";
 
 export class UserServiceImpl implements UserServiceInterface
 {
 	Login(request: LoginRequest): LoginRequest
+	{
+		return null;
+	}
+
+	Register(request: RegisterRequest): RegisterRequest
 	{
 		return null;
 	}
--- backend/user/UserServiceInterface.ts
+++ backend/user/UserServiceInterface.ts
@@ -1,7 +1,11 @@
 import {LoginRequest} from "./request/LoginRequest";
 import {LoginResponse} from "./response/LoginResponse";
+import {RegisterRequest} from "./request/RegisterRequest";
+import {RegisterResponse} from "./response/RegisterResponse";
 
 export interface UserServiceInterface
 {
 	Login(request: LoginRequest): LoginResponse;
+
+	Register(request: RegisterRequest): RegisterRequest
 }
