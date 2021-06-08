d8e894c0745c3cb9214ecf2fcba3dbca10a211cf
Luca <Lucaprenzler1@gmail.com>
2021-06-08 20:11:56 +0200

    Login functionality with the API implemented in the index.ts file

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,25 @@
-const express = require( 'express' );
-const path = require('path');
+//const express = require( 'express' );
+//const path = require('path');
+//const app = express();
+
+//app.use('api/login', require('./routes/loginRoutes'));
+
+//const port = process.env.port || 8080; // default port to listen
+//app.listen(port);
+
+
+//___________________________________________________________________________________________________________________
+//_________________Luca Implementation of the request and response objects___________________________________________
+//____________________________________________________________________________________________________________________
+
+// @ts-ignore
+import * as functions from 'firebase-functions';
+// @ts-ignore
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+
+admin.initializeApp();
+
 const app = express();
 
-app.use('api/login', require('./routes/loginRoutes'));
-
-const port = process.env.port || 8080; // default port to listen
-app.listen(port);
-
-
+export const api = functions.https.onRequest(app);
