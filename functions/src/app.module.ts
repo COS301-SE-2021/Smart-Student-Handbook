08f0d8c248ba7ba6d4a99884d1dd2c6eb7530a34
Theo <u19002514@tuks.co.za>
2021-06-11 18:23:09 +0200

    generated user controller + service

--- functions/src/app.module.ts
+++ functions/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationController } from './notification/notification.controller';
 import { NotificationService } from './notification/notification.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, NotificationController],
-  providers: [AppService, NotificationService],
+  controllers: [AppController, NotificationController, UserController],
+  providers: [AppService, NotificationService, UserService],
 })
 export class AppModule {}
