aef23764290f8166e9d001704f115e7cc63d4614
Theo <u19002514@tuks.co.za>
2021-06-11 12:51:23 +0200

    generated notification controller

--- functions/src/app.module.ts
+++ functions/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationController } from './notification/notification.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, NotificationController],
   providers: [AppService],
 })
 export class AppModule {}
