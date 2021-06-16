295c9d8f74aca270d61ce514d9c070611f3f6485
Anro <u19324058@tuks.co.za>
2021-06-16 23:16:37 +0200

    Revert "Added sendSingle, sendGroup notification + subscribe to topic"
    This reverts commit f3f91842f270d6092d9719a6ca28357074a39198.

--- functions/src/notification/notification.controller.ts
+++ functions/src/notification/notification.controller.ts
@@ -1,24 +1,17 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
-import { EmailNotificationResponseDto } from './dto/emailNotificationResponse.dto';
-import { EmailNotificationRequestDto } from './dto/emailNotificationRequest.dto';
-import { NotificationService } from './notification.service';
+import { Controller, Post, Body } from '@nestjs/common';
+import { EmailNotificationResponseDto} from "./dto/emailNotificationResponse.dto";
+import { EmailNotificationRequestDto } from "./dto/emailNotificationRequest.dto"
+import { NotificationService } from "./notification.service";
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+	constructor(private readonly notificationService: NotificationService) {
+	}
 
-  @Post()
-  async sendEmailNotification(
-    @Body() emailNotificationDto: EmailNotificationRequestDto,
-  ): Promise<EmailNotificationResponseDto> {
-    return await this.notificationService.sendEmailNotification(
-      emailNotificationDto,
-    );
-  }
+	@Post()
+	async sendEmailNotification(@Body() emailNotificationDto: EmailNotificationRequestDto): Promise<EmailNotificationResponseDto> {
+		return await this.notificationService.sendEmailNotification(emailNotificationDto);
+	}
 
-  @Get()
-  async sendPushNotification(){
-    await this.notificationService.sendPushNotification();
-    return true;
-  }
 }
+
