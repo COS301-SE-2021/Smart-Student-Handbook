56e55fa0f319f9beb3dc42a88dd3ce57c93c7c6e
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-06-17 14:41:53 +0200

    Auth service and controller setup complete

--- functions/src/app.module.ts
+++ functions/src/app.module.ts
@@ -7,10 +7,12 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { NotebookController } from './notebook/notebook.controller';
 import { NotebookService } from './notebook/notebook.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, NotificationController, UserController, NotebookController],
-  providers: [AppService, NotificationService, UserService, NotebookService],
+  controllers: [AppController, NotificationController, UserController, NotebookController, AuthController],
+  providers: [AppService, NotificationService, UserService, NotebookService, AuthService],
 })
 export class AppModule {}
