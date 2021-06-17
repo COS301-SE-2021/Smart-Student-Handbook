b35041c03f75e6a1258f02eb2b8ad66f8cd3c62c
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-06-17 14:57:23 +0200

    Changed Service to Services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,7 +34,7 @@ import { AngularFireMessagingModule } from '@angular/fire/messaging';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
-import { MessagingService } from './service/messaging.service';
+import { MessagingService } from './services/messaging.service';
 import { environment } from '../environments/environment';
 import { AsyncPipe } from '../../node_modules/@angular/common';
 
