b4649a6dfd93d2a19501972fd2874c8e0d49eb0c
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-06-17 20:11:20 +0200

    User DTO completed

---
+++ functions/src/user/dto/user.dto.ts
@@ -0,0 +1,11 @@
+export class User{
+    readonly uid:string
+    readonly name?:string
+    readonly username:string
+    readonly institution?:string
+    readonly department?:string
+    readonly program?:string
+    readonly workStatus?:string
+    readonly bio?:string
+    readonly dateJoined:string
+}
