8fa792096c638144b63301f195cae78ed43039a4
LouwC <u18000402@tuks.co.za>
2021-06-18 15:58:35 +0200

    Updated files to be able to user account controller and sevice

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 const dotenv = require('dotenv');
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
+import firebase from "firebase/app";
 dotenv.config();
 
 const config: ServiceAccount = {
@@ -18,10 +19,21 @@ export async function createNestServer() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 5001);
   admin.initializeApp({
-    credential: admin.credential.cert(config),
-    databaseURL:
-      'https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app',
+    credential: admin.credential.applicationDefault(),
   });
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyAFpQOCQy42NzigYd5aPH3OSpbjvADJ0o0",
+    authDomain: "smartstudentnotebook.firebaseapp.com",
+    databaseURL: "https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "smartstudentnotebook",
+    storageBucket: "smartstudentnotebook.appspot.com",
+    messagingSenderId: "254968215542",
+    appId: "1:254968215542:web:be0931c257ad1d8a60b9d7",
+    measurementId: "G-YDRCWDT5QJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
   // console.log(config);
 }
