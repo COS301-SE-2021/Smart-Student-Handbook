664e91794565a255300fbe71120dc9562c27cc1f
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-06-18 11:21:56 +0200

    getUserDetails Service Completed

--- functions/src/user/user.service.ts
+++ functions/src/user/user.service.ts
@@ -1,6 +1,8 @@
-import {Injectable} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import * as admin from "firebase-admin";
+import firebase  from "firebase/app";
 import {UserRequestDto} from "./dto/userRequest.dto";
-import {UserResponseDto} from "./dto/userResponse.dto";
+import {User, UserResponseDto} from "./dto/userResponse.dto";
 
 @Injectable()
 export class UserService {
@@ -8,12 +10,33 @@ export class UserService {
 
     async getUserDetails(uid: string): Promise<UserResponseDto>
     {
-        return {success: false, message: "An unexpected Error Occurred"};
+        let requestedUser: User;
+        const userRef = admin.firestore().collection("users").where("uid", "==", uid);
+        const snapshot = await userRef.get();
+        snapshot.forEach(doc => {
+             requestedUser =
+                {
+                    uid: uid,
+                    name:doc.data()["name"],
+                    username:doc.data()["username"],
+                    institution:doc.data()["institution"],
+                    department:doc.data()["department"],
+                    program:doc.data()["program"],
+                    workStatus:doc.data()["workStatus"],
+                    bio:doc.data()["bio"],
+                    dateJoined:doc.data()["dateJoined"],
+                }
+        });
+
+        if(requestedUser === null) {
+            throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+        }
+
+        return {success: true, message: "User was successfully found",userInfo:requestedUser};
     }
 
     async createAndUpdateUser(user: UserRequestDto, update: boolean = false): Promise<UserResponseDto>
     {
-        let response: UserResponseDto;
 
         if (update)
         {
