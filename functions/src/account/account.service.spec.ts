8324b9bb454c7097f02b972f4baffc5e9548decd
Luca <Lucaprenzler1@gmail.com>
2021-06-18 19:27:29 +0200

    Added a unit test for RegisterUnit added

--- functions/src/account/account.service.spec.ts
+++ functions/src/account/account.service.spec.ts
@@ -15,4 +15,14 @@ describe('AccountService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  //test registerUser
+  describe('registerUser' , ()=>{
+    describe('The user will enter their details' , ()=>{
+      it('If all user details are entered correctly the user will be registered' , async()=>{
+
+      })
+    })
+  })
+
 });
