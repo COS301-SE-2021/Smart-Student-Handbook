5df0c85af2358e47f9784b1240d806ceb3c08090
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-08-03 23:25:46 +0200

    Menu Links added

--- frontend/src/app/core/components/left-menu/left-menu.component.ts
+++ frontend/src/app/core/components/left-menu/left-menu.component.ts
@@ -11,12 +11,6 @@ import {
 import { EditProfileComponent, TreeViewComponent } from '@app/components';
 import { animateText, onSideNavChange } from '@app/styling/animations';
 
-interface Page {
-	link: string;
-	name: string;
-	icon: string;
-}
-
 @Component({
 	selector: 'app-left-menu',
 	templateUrl: './left-menu.component.html',
@@ -25,13 +19,6 @@ interface Page {
 	animations: [onSideNavChange, animateText],
 })
 export class LeftMenuComponent implements OnInit {
-	public pages: Page[] = [
-		{ name: 'Inbox', link: 'some-link', icon: 'inbox' },
-		{ name: 'Starred', link: 'some-link', icon: 'star' },
-		{ name: 'Send email', link: 'some-link', icon: 'send' },
-		{ name: 'Logout', link: 'logout()', icon: 'exit_to_app' },
-	];
-
 	public sideNavState: boolean = false;
 
 	public linkText: boolean = false;
