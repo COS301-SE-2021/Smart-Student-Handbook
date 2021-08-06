7b74938949dbefe715d8f72014e28ab74b315749
Anro <u19324058@tuks.co.za>
2021-08-06 14:53:28 +0200

    Merge branch 'feature/frontend/menu' into feature/frontend/pwa + added notifications page

--- frontend/src/app/core/material.module.ts
+++ frontend/src/app/core/material.module.ts
@@ -87,7 +87,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 		MatMenuModule,
 		MatSidenavModule,
 		MatToolbarModule,
-		// MatListModule,
+		MatListModule,
 		// MatGridListModule,
 		MatCardModule,
 		// MatStepperModule,
