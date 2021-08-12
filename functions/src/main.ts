db0694037d45a6eb8f2da3c8646eabc40a1ada4d
Theo <u19002514@tuks.co.za>
2021-08-12 14:27:34 +0200

    updated main.ts

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -46,29 +46,31 @@ createNestServer(server)
 export const app = functions.https.onRequest(server);
 
 export const api = functions.https.onRequest((req, res) => {
-	res.send('Functionn are Working!!');
+	res.send('Functions are Working!!');
 });
 
-// const APP_ID = functions.config().algolia.app;
-// const ADMIN_KEY = functions.config().algolia.key;
-//
-// const client = algoliasearch(APP_ID, ADMIN_KEY);
-// const index = client.initIndex('userNotebooks');
-//
-// exports.addNotebookIndex = functions.firestore.document('userNotebooks/{notebookId}').onCreate((snapshot) => {
-// 	const data = snapshot.data();
-// 	const objectID = snapshot.id;
-//
-// 	index.saveObject({ data, objectID });
-// });
-//
-// exports.updateNotebookIndex = functions.firestore.document('userNotebooks/{userNotebookId}').onUpdate((change) => {
-// 	const newData = change.after.data();
-// 	const objectID = change.after.id;
-//
-// 	index.saveObject({ newData, objectID });
-// });
-//
-// exports.deleteNotebookIndex = functions.firestore
-// 	.document('userNotebooks/{userNotebookId}')
-// 	.onDelete((snapshot) => index.deleteObject(snapshot.id));
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
+
+console.log(functions.config());
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('userNotebooks');
+
+exports.addNotebookIndex = functions.firestore.document('userNotebooks/{notebookId}').onCreate((snapshot) => {
+	const data = snapshot.data();
+	const objectID = snapshot.id;
+
+	index.saveObject({ data, objectID });
+});
+
+exports.updateNotebookIndex = functions.firestore.document('userNotebooks/{userNotebookId}').onUpdate((change) => {
+	const newData = change.after.data();
+	const objectID = change.after.id;
+
+	index.saveObject({ newData, objectID });
+});
+
+exports.deleteNotebookIndex = functions.firestore
+	.document('userNotebooks/{userNotebookId}')
+	.onDelete((snapshot) => index.deleteObject(snapshot.id));
