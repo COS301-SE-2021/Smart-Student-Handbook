85053ea8dc27439d2912466de8f6a70b713d0908
Luca <Lucaprenzler1@gmail.com>
2021-08-15 23:49:34 +0200

    algolia implemented and working on frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
+
+const searchClient = algoliasearch(
+	'B1G2GM9NG0',
+	'aadef574be1f9252bb48d4ea09b5cfe5'
+);
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+	config = {
+		indexName: 'demo_ecommerce',
+		searchClient,
+	};
+}
--- frontend/src/app/features/explore/explore.component.ts
+++ frontend/src/app/features/explore/explore.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import algoliasearch from 'algoliasearch/lite';
 
+const searchClient = algoliasearch(
+	'AD2K8AK74A',
+	'589f047ba9ac7fa58796f394427d7f35'
+);
 @Component({
 	selector: 'app-explore',
 	templateUrl: './explore.component.html',
 	styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
+	config = {
+		apiKey: '589f047ba9ac7fa58796f394427d7f35',
+		appId: 'AD2K8AK74A',
+		indexName: 'users',
+		routing: true,
+		searchClient,
+	};
 	// constructor() {}
 
 	// eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
