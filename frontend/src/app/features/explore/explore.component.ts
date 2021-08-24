204f68cc3031462629225da9c48affce36209fb1
Luca <Lucaprenzler1@gmail.com>
2021-08-24 20:53:20 +0200

    added algolia to the explore page to capture user events

--- frontend/src/app/features/explore/explore.component.ts
+++ frontend/src/app/features/explore/explore.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import algoliasearch from 'algoliasearch/lite';
+// import aa from 'search-insights';
 
 const searchClient = algoliasearch(
 	'AD2K8AK74A',
 	'589f047ba9ac7fa58796f394427d7f35'
 );
+
 @Component({
 	selector: 'app-explore',
 	templateUrl: './explore.component.html',
