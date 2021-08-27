72dfbf4368e959075e35d6f1375caee44778d17f
Luca <Lucaprenzler1@gmail.com>
2021-08-27 16:33:05 +0200

    events created for click evvents

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import algoliasearch from 'algoliasearch/lite';
-
-const searchClient = algoliasearch(
-	'B1G2GM9NG0',
-	'aadef574be1f9252bb48d4ea09b5cfe5'
-);
 
 @Component({
 	selector: 'app-root',
@@ -13,9 +7,4 @@ const searchClient = algoliasearch(
 })
 export class AppComponent {
 	title: string = 'smart-student';
-
-	config = {
-		indexName: 'demo_ecommerce',
-		searchClient,
-	};
 }
