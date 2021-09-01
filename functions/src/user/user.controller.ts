6cfd8c6bd73d9964f0a00d2ce56334497d8f4f32
LouwC <u18000402@tuks.co.za>
2021-09-01 16:41:21 +0200

    Fixed an error

--- functions/src/user/user.controller.ts
+++ functions/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete, Body, Param, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Headers } from '@nestjs/common';
 import { UserRequestDto } from './dto/userRequest.dto';
 import { UserResponseDto } from './dto/userResponse.dto';
 import { UserService } from './user.service';
