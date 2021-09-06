91737588d7ea6c7759b46924912abf4e5426b137
Douglas <62644072+Douglas6312@users.noreply.github.com>
2021-09-06 23:22:04 +0200

    logout when token expires in the Interceptor and Auth Guard

--- frontend/src/app/core/jwt.interceptor.ts
+++ frontend/src/app/core/jwt.interceptor.ts
@@ -7,28 +7,48 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AccountService } from '@app/services';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
+
+const jwtHelper = new JwtHelperService();
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-	constructor(private accountService: AccountService) {}
+	constructor(
+		private accountService: AccountService,
+		private router: Router
+	) {}
 
 	intercept(
 		request: HttpRequest<unknown>,
 		next: HttpHandler
 	): Observable<HttpEvent<unknown>> {
-		// add auth header with jwt if user is logged in and request is to the api url
-		// const user = this.accountService.userValue;
-		// const isLoggedIn = user && user.token;
-
 		const authToken = JSON.parse(<string>localStorage.getItem('authToken'));
 
-		if (this.accountService.getLoginState) {
-			// eslint-disable-next-line no-param-reassign
-			request = request.clone({
-				setHeaders: {
-					token: `${authToken}`,
-				},
-			});
+		if (authToken) {
+			// const decodedToken = jwtHelper.decodeToken(authToken);
+			// const expirationDate = jwtHelper.getTokenExpirationDate(authToken);
+			const isExpired = jwtHelper.isTokenExpired(authToken);
+			if (!isExpired) {
+				if (this.accountService.getLoginState) {
+					// eslint-disable-next-line no-param-reassign
+					request = request.clone({
+						setHeaders: {
+							token: `${authToken}`,
+						},
+					});
+				}
+			} else {
+				// TODO refresh the token here
+				this.accountService.singOut().subscribe(
+					() => {
+						this.router.navigate(['account/login']);
+					},
+					(err) => {
+						console.log(`Error: ${err.error.message}`);
+					}
+				);
+			}
 		}
 
 		return next.handle(request);
