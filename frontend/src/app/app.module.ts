53b776c99b61f5dc26edbf3291f1d510f0d4ad0d
Anro <u19324058@tuks.co.za>
2021-09-11 15:12:45 +0200

    Updated landing page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -87,7 +87,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
 	declarations: [
 		AppComponent,
--- frontend/src/app/features/public/landing-page/landing-page.component.ts
+++ frontend/src/app/features/public/landing-page/landing-page.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
+import {
+	AfterContentChecked,
+	AfterContentInit,
+	AfterViewChecked,
+	AfterViewInit,
+	Component,
+	OnInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '@app/services';
+import * as AOS from 'aos';
 
 @Component({
 	selector: 'app-landing-page',
 	templateUrl: './landing-page.component.html',
 	styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent {
+export class LandingPageComponent
+	implements
+		OnInit,
+		AfterViewInit,
+		AfterContentInit,
+		AfterViewChecked,
+		AfterContentChecked
+{
+	teamMembers: any[] = [
+		{
+			name: 'Arno Möller',
+			linkedIn: 'https://www.linkedin.com/in/arno-m%C3%B6ller-a96a8920b/',
+			github: 'https://github.com/Arno-Moller',
+			profile:
+				'url(../../../../assets/logo/SmartStudentHandbookLogo.png)',
+		},
+		{
+			name: 'Douglas van Reeuwyk',
+			linkedIn: 'https://www.linkedin.com/in/douglasvanreeuwyk/',
+			github: 'https://github.com/Douglas6312',
+			profile:
+				'url(../../../../assets/logo/SmartStudentHandbookLogo.png)',
+		},
+		{
+			name: 'Louw Claassens',
+			linkedIn:
+				'https://www.linkedin.com/in/aj-louw-claassens-2b296a19a/',
+			github: 'https://github.com/LouwC',
+			profile:
+				'url(../../../../assets/logo/SmartStudentHandbookLogo.png)',
+		},
+		{
+			name: 'Luca Prenzler',
+			linkedIn: 'https://www.linkedin.com/in/luca-prenzler-5aaa0920b/',
+			github: 'https://github.com/LucaPrenzler',
+			profile:
+				'url(../../../../assets/logo/SmartStudentHandbookLogo.png)',
+		},
+		{
+			name: 'Theo Morkel',
+			linkedIn: 'https://www.linkedin.com/in/theo-morkel-197610206',
+			github: 'https://github.com/u19002514-Theo-Morkel',
+			profile:
+				'url(../../../../assets/logo/SmartStudentHandbookLogo.png)',
+		},
+	];
+
 	constructor(
 		private router: Router,
 		private accountService: AccountService
@@ -16,5 +70,30 @@ export class LandingPageComponent {
 		if (this.accountService.getLoginState) {
 			this.router.navigate(['/home']);
 		}
+	}
+
+	ngOnInit(): void {
+		AOS.init({
+			easing: 'ease-out-back',
+			duration: 1000,
+		});
+
+		window.addEventListener('load', AOS.refresh);
+	}
+
+	ngAfterViewInit(): void {
+		// AOS.refresh();
+	}
+
+	ngAfterContentInit(): void {
+		// AOS.refresh();
+	}
+
+	ngAfterViewChecked(): void {
+		// AOS.refresh();
+	}
+
+	ngAfterContentChecked(): void {
+		// AOS.refresh();
 	}
 }
