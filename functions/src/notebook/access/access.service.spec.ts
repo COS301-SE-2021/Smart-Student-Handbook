eac8b38fc3d3c2f50db166f39bcd9001b46eb91e
LouwC <u18000402@tuks.co.za>
2021-09-14 19:29:43 +0200

    Fixed add access bug

--- functions/src/notebook/access/access.service.spec.ts
+++ functions/src/notebook/access/access.service.spec.ts
@@ -2,17 +2,17 @@
 // import { AccessService } from './access.service';
 //
 // describe('AccessService', () => {
-//   let service: AccessService;
+// 	let service: AccessService;
 //
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AccessService],
-//     }).compile();
+// 	beforeEach(async () => {
+// 		const module: TestingModule = await Test.createTestingModule({
+// 			providers: [AccessService],
+// 		}).compile();
 //
-//     service = module.get<AccessService>(AccessService);
-//   });
+// 		service = module.get<AccessService>(AccessService);
+// 	});
 //
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
+// 	it('should be defined', () => {
+// 		expect(service).toBeDefined();
+// 	});
 // });
