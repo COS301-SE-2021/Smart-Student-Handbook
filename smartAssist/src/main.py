a47c80c4be8300c2184513e1a0b01f4590315508
Theo <u19002514@tuks.co.za>
2021-09-16 15:05:33 +0200

    improved our smartAssist model

--- smartAssist/src/main.py
+++ smartAssist/src/main.py
@@ -7,11 +7,13 @@ import numpy as np
 
 # data = SmartAssistData()
 
+# cloud.loadAllData()
+
 # data.loadData(count=10000)
 
-# # data.generateFinalData(n_positive=5000)
+# data.generateFinalData(n_positive=5000)
 
-# # data.generateTrainTestData()
+# data.generateTrainTestData()
 
 # smartmodel = SmartAssistModel(data=data)
 
@@ -21,29 +23,27 @@ import numpy as np
 
 # print("training")
 
-# # xtrain = {
-# #     'data': np.asarray(data.finalSetXTrain[:,0]).astype('float32'), 
-# #     'name': np.asarray(data.finalSetXTrain[:,1]).astype('float32'), 
-# #     'tags': np.asarray(data.finalSetXTrain[:,2]).astype('float32'), 
-# #     'author': np.asarray(data.finalSetXTrain[:,3]).astype('float32'), 
-# #     'institution': np.asarray(data.finalSetXTrain[:,4]).astype('float32'),
-# #     'course': np.asarray(data.finalSetXTrain[:,5]).astype('float32'),
-# #     'soup': np.asarray(data.finalSetXTrain[:,6].tolist()).astype('float32')
-# #     }
+# xtrain = {
+#     'data': np.asarray(data.finalSetXTrain[:,0]).astype('float32'), 
+#     'name': np.asarray(data.finalSetXTrain[:,1]).astype('float32'), 
+#     'tags': np.asarray(data.finalSetXTrain[:,2]).astype('float32'), 
+#     'author': np.asarray(data.finalSetXTrain[:,3]).astype('float32'), 
+#     'institution': np.asarray(data.finalSetXTrain[:,4]).astype('float32'),
+#     'course': np.asarray(data.finalSetXTrain[:,5]).astype('float32')
+#     }
 
-# # ytrain = data.finalSetYTrain
+# ytrain = data.finalSetYTrain
 
-# # xtest = {
-# #     'data': np.asarray(data.finalSetXTest[:,0]).astype('float32'), 
-# #     'name': np.asarray(data.finalSetXTest[:,1]).astype('float32'), 
-# #     'tags': np.asarray(data.finalSetXTest[:,2]).astype('float32'), 
-# #     'author': np.asarray(data.finalSetXTest[:,3]).astype('float32'), 
-# #     'institution': np.asarray(data.finalSetXTest[:,4]).astype('float32'),
-# #     'course': np.asarray(data.finalSetXTest[:,5]).astype('float32'),
-# #     'soup': np.asarray(data.finalSetXTest[:,6].tolist()).astype('float32')
-# #     }
+# xtest = {
+#     'data': np.asarray(data.finalSetXTest[:,0]).astype('float32'), 
+#     'name': np.asarray(data.finalSetXTest[:,1]).astype('float32'), 
+#     'tags': np.asarray(data.finalSetXTest[:,2]).astype('float32'), 
+#     'author': np.asarray(data.finalSetXTest[:,3]).astype('float32'), 
+#     'institution': np.asarray(data.finalSetXTest[:,4]).astype('float32'),
+#     'course': np.asarray(data.finalSetXTest[:,5]).astype('float32')
+#     }
 
-# # ytest = data.finalSetYTest
+# ytest = data.finalSetYTest
 
 # smartmodel.train()
 
@@ -51,8 +51,8 @@ import numpy as np
 
 # # # smartmodel.loadSmartModel()
 
-# # smartmodel.trainModel(dataX= xtrain, dataY= ytrain, validationData= ())
-# # smartmodel.evaluteModel(dataX= xtest, dataY= ytest)
+# smartmodel.trainModel(dataX= xtrain, dataY= ytrain, validationData= (), batch_size=128, epochs=32, steps_per_epoch=16)
+# smartmodel.evaluteModel(dataX= xtest, dataY= ytest)
 
 # # smartmodel.loadSmartModel()
 
@@ -87,13 +87,6 @@ import numpy as np
 # # smartmodel.saveEmbeddingWeights()
 # # print(smartmodel.predict(item))
 
-cloud = cloudStorage()
-data = SmartAssistData()
-smartmodel = SmartAssistModel(data=data)
 
 
-cloud.loadAllData()
 
-smartmodel.train()
-
-cloud.saveAllData()
