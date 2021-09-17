3891e5db475b98151f5f3fe5687f4bac21d3b3fd
LouwC <u18000402@tuks.co.za>
2021-09-17 23:18:48 +0200

    Completed the auth service testing

--- functions/src/auth/auth.service.spec.ts
+++ functions/src/auth/auth.service.spec.ts
@@ -1,22 +1,71 @@
-describe('AccountService', () => {
-	it('Do Tests', async () => {});
+import { Test, TestingModule } from '@nestjs/testing';
+import firebase from 'firebase';
+import * as admin from 'firebase-admin';
+import { HttpException } from '@nestjs/common';
+import { AccountService } from '../account/account.service';
+import { UserService } from '../user/user.service';
+import { NotificationService } from '../notification/notification.service';
+import { AuthService } from './auth.service';
+
+const serviceAccount = require('../../service_account.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app',
 });
 
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AuthService } from './auth.service';
-//
-// describe('AuthService', () => {
-//   let service: AuthService;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthService],
-//     }).compile();
-//
-//     service = module.get<AuthService>(AuthService);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+const firebaseConfig = {
+	apiKey: 'AIzaSyAFpQOCQy42NzigYd5aPH3OSpbjvADJ0o0',
+	authDomain: 'smartstudentnotebook.firebaseapp.com',
+	databaseURL: 'https://smartstudentnotebook-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'smartstudentnotebook',
+	storageBucket: 'smartstudentnotebook.appspot.com',
+	messagingSenderId: '254968215542',
+	appId: '1:254968215542:web:be0931c257ad1d8a60b9d7',
+	measurementId: 'G-YDRCWDT5QJ',
+};
+firebase.initializeApp(firebaseConfig);
+
+describe('Auth Service Tests', () => {
+	let accountService: AccountService;
+	let authService: AuthService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [],
+			providers: [AccountService, AuthService, UserService, NotificationService],
+		}).compile();
+		//
+		accountService = module.get<AccountService>(AccountService);
+		authService = module.get<AuthService>(AuthService);
+
+		jest.setTimeout(30000);
+	});
+
+	describe('Login user to be used', () => {
+		it('Should login a user successfully', async () => {
+			const user = {
+				email: 'TestUserAccount@gmail.com',
+				password: 'TestPassword!0',
+			};
+
+			const userData = await accountService.loginUser(user);
+
+			const result = await authService.verifyUser(userData.authToken);
+
+			expect(result).toBe('sYTwoaCyHQO0cNAqJcxBnO70yne2');
+		});
+
+		it('Throw Exception: Invalid JWT token', async () => {
+			let error: HttpException;
+
+			try {
+				await authService.verifyUser('Invalid JWT token');
+			} catch (e) {
+				error = e;
+			}
+
+			expect(error.message).toBe('Was unable to authenticate user. Invalid token!');
+		});
+	});
+});
