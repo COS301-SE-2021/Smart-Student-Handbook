cf6e71eb12f43dbdd6eb13069e22dcd522c9ce11
LouwC <u18000402@tuks.co.za>
2021-09-18 12:43:43 +0200

    Updated notification integration testing

--- functions/src/notification/notification.service.integration.spec.ts
+++ functions/src/notification/notification.service.integration.spec.ts
@@ -3,6 +3,9 @@ import firebase from 'firebase';
 import * as admin from 'firebase-admin';
 import { NotificationService } from './notification.service';
 import { EmailInterface } from './interfaces/email.interface';
+import { SingleNotificationRequestDto } from './dto/singleNotificationRequest.dto';
+import { SendNotificationToGroupRequestDto } from './dto/sendNotificationToGroup.dto';
+import { SubscribeToTopicRequestDto } from './dto/subscribeToTopicRequest.dto';
 
 const serviceAccount = require('../../service_account.json');
 
@@ -25,6 +28,7 @@ firebase.initializeApp(firebaseConfig);
 
 describe('Notifications Service Integration Tests', () => {
 	let notificationService: NotificationService;
+	const userId = '0iIh3PKlKsTt702mxlbQyd1ZCG53';
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -42,16 +46,53 @@ describe('Notifications Service Integration Tests', () => {
 		});
 	});
 
-	describe('Setup Notebook for testing', () => {
-		it('Test successfully create a notebook', async () => {
-			const email: EmailInterface = {
+	describe('Email Notification Testing', () => {
+		it('Send Email Notification', async () => {
+			const emailInterface: EmailInterface = {
 				email: 'louw707@gmail.com',
 				subject: 'Notifications test subject',
 				body: 'Notifications test body',
 			};
-			const result = await notificationService.sendEmailNotification(email);
+			const result = await notificationService.sendEmailNotification(emailInterface);
 
 			expect(result.success).toBe(true);
+		});
+	});
+
+	describe('PushNotification Testing', () => {
+		it('Test Send Notification To Group', async () => {
+			const sendNotificationToGroupRequestDto: SendNotificationToGroupRequestDto = {
+				topic: 'Notifications-test-topic',
+				title: 'Notifications test title',
+				body: 'Notifications test body',
+				userId,
+			};
+			const result = await notificationService.sendGroupPushNotification(sendNotificationToGroupRequestDto);
+
+			expect(result.status).toBe('successful');
+		});
+
+		it('Test Single Notification', async () => {
+			const singleNotificationRequestDto: SingleNotificationRequestDto = {
+				token: 'invalidToken',
+				title: 'Notifications test title',
+				body: 'Notifications test body',
+				userId,
+			};
+			const result = await notificationService.sendSinglePushNotification(singleNotificationRequestDto);
+
+			expect(result.status).toBe('unsuccessful');
+		});
+
+		it('Test Subscribe To Notification Topic', async () => {
+			const subscribeToTopicRequestDto: SubscribeToTopicRequestDto = {
+				token: 'invalidToken',
+				topic: 'Notifications-test-topic',
+				userId,
+			};
+			const result = await notificationService.subscribeToNotificationTopic(subscribeToTopicRequestDto);
+
+			expect(result.status).toBe('unsuccessful');
 		});
 	});
 });
