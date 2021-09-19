45d9eee7a10afee8bdb7e7ec0d9ecb9ef9cfde0b
Luca <Lucaprenzler1@gmail.com>
2021-09-20 01:56:28 +0200

    added unit tests for note.service.ts

--- functions/src/notebook/note/note.service.spec.ts
+++ functions/src/notebook/note/note.service.spec.ts
@@ -1,21 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NoteService } from './note.service';
+
 describe('AccountService', () => {
 	it('Do Tests', async () => {});
 });
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { NoteService } from './note.service';
-//
-// describe('NoteService', () => {
-//   let service: NoteService;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [NoteService],
-//     }).compile();
-//
-//     service = module.get<NoteService>(NoteService);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+
+describe('NoteService', () => {
+	let service: NoteService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [NoteService],
+		}).compile();
+
+		service = module.get<NoteService>(NoteService);
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+});
